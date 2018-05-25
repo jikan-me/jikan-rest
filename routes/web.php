5ea871e89d9b8a94fbd6c192d0edea2ebda7a55c
irfan-dahir <irfan.dahir@gmail.com>
2018-05-25 06:59:35 +0500

    add IP Blacklist Middleware

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ $router->get('meta/{request:[A-Za-z]+}[/{type:[A-Za-z]+}[/{period:[A-Za-z]+}[/{p
 	'uses' => 'MetaLiteController@request'
 ]);
 
-$router->group(['middleware' => ['meta', 'throttle']], function() use ($router) {
+$router->group(['middleware' => ['blacklist', 'meta', 'throttle']], function() use ($router) {
 
 	$router->get('anime[/{id:[0-9]+}[/{extend:[A-Za-z_]+}[/{extendArgs}]]]', [
 		'uses' => 'AnimeController@request'
