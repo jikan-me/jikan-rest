cfae2906a96a39d019692fb5375975d9abb9e19a
irfan-dahir <irfan.dahir@gmail.com>
2018-08-26 04:13:25 +0500

    v2: add Character endpoints

--- app/Http/Controllers/V2/CharacterController.php
+++ app/Http/Controllers/V2/CharacterController.php
@@ -7,15 +7,46 @@ use Jikan\Request\Character\CharacterPicturesRequest;
 
 class CharacterController extends Controller
 {
+    public function _main($id) {
+        $character = $this->jikan->getCharacter(new CharacterRequest($id));
+
+        // backwards compatibility
+        $character = json_decode(
+            $this->serializer->serialize($character, 'json'),
+            true
+        );
+
+        $character['nicknames'] = empty($character['nicknames']) ? null : implode(",", $character['nicknames']);;
+
+        return $character;
+    }
+
     public function main(int $id)
     {
-        $character = $this->jikan->getCharacter(new CharacterRequest($id));
-        return response($this->serializer->serialize($character, 'json'));
+        $character = $this->_main($id);
+
+        return response($character);
     }
 
     public function pictures(int $id)
     {
-        $character = $this->jikan->getCharacterPictures(new CharacterPicturesRequest($id));
-        return response($this->serializer->serialize($character, 'json'));
+        $character = $this->_main($id);
+        $pictures = ['image' =>$this->jikan->getCharacterPictures(new CharacterPicturesRequest($id))];
+        $pictures = json_decode(
+            $this->serializer->serialize($pictures, 'json'),
+            true
+        );
+
+        foreach($pictures['image'] as $key => $value) {
+            $pictures['image'][$key] = $value['small'];
+        }
+
+
+        return response(
+            array_merge(
+                $character,
+                $pictures
+            )
+        );
     }
 }
