4e116c7a8d534a92eee23c97fede9a7d86a6e36c
Irfan <irfan.dahir@protonmail.com>
2019-04-26 02:20:32 +0500

    add v4 controllers & router

---
+++ app/Http/Controllers/V4/Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\V3;
+
+use App\Providers\SerializerFactory;
+use App\Providers\SerializerServiceProdivder;
+use App\Providers\SerializerServiceProviderV3;
+use Jikan\Jikan;
+use Jikan\MyAnimeList\MalClient;
+use JMS\Serializer\Context;
+use JMS\Serializer\Serializer;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+
+class Controller extends BaseController
+{
+    /**
+     * @var Serializer
+     */
+    protected $serializer;
+
+    /**
+     * @var MalClient
+     */
+    protected $jikan;
+
+    /**
+     * AnimeController constructor.
+     *
+     * @param Serializer      $serializer
+     * @param MalClient $jikan
+     */
+    public function __construct(MalClient $jikan)
+    {
+        $this->serializer = SerializerFactory::createV3();
+        $this->jikan = $jikan;
+    }
+}
