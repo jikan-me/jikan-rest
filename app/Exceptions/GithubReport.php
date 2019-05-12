fac19b5c5b1b8c2ee4638a5c5be45da9ad5946fd
Irfan <irfan.dahir@protonmail.com>
2019-05-12 18:40:15 +0500

    improve GithubReport.php #47

--- app/Exceptions/GithubReport.php
+++ app/Exceptions/GithubReport.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Http\Request;
+use PackageVersions\Versions;
 use Predis\Connection\ConnectionException;
 
 /**
@@ -39,6 +40,31 @@ class GithubReport
     private $repo;
 
     /**
+     * @var string
+     */
+    private $trace;
+
+    /**
+     * @var string
+     */
+    private $jikanVersion;
+
+    /**
+     * @var string
+     */
+    private $redisRunning;
+
+    /**
+     * @var string
+     */
+    private $instanceType;
+
+    /**
+     * @var
+     */
+    private $phpVersion;
+
+    /**
      * @param \Exception $exception
      * @param Request $request
      * @return string
@@ -49,9 +75,14 @@ class GithubReport
         $report->name = \get_class($exception);
         $report->code = $exception->getCode();
         $report->error = $exception->getMessage();
+        $report->trace = "{$exception->getFile()} on line {$exception->getLine()}";
         $report->repo = $repo ?? env('GITHUB_REST', 'jikan-me/jikan-rest');
         $report->requestUri = $request->getRequestUri();
         $report->requestMethod = $request->getMethod();
+        $report->jikanVersion = Versions::getVersion('jikan-me/jikan');
+        $report->redisRunning = trim(app('redis')->ping()) === 'PONG' ? "Connected" : "Disconnected";
+        $report->instanceType = $_SERVER['SERVER_NAME'] === 'api.jikan.moe' ? 'OFFICIAL' : 'HOSTED';
+        $report->phpVersion = PHP_VERSION;
 
         return $report;
     }
@@ -61,9 +92,9 @@ class GithubReport
      */
     public function __toString() : string
     {
-        $title = urlencode("Generated Issue: {$this->getClassName()}");
+        $title = urlencode("[{$this->instanceType}] Generated Issue: {$this->getClassName()}");
         $body = urlencode(
-            "**Exception:** `{$this->name}`\n**Code:** `{$this->code}`\n**Message:** `{$this->error}`\n**Request:** `{$this->requestMethod} {$this->requestUri}`\n\n**Enter Addition Details:**\n"
+            "Please fill out the details below.\n\n**Summary:**\n\n**Steps to reproduce:**\n\n\n\n ### Additional Details \n **Jikan Parser Version**: ```{$this->jikanVersion}```\n**PHP:** ```{$this->phpVersion}```\n**Redis**: ```{$this->redisRunning}```\n**Exception:** ```{$this->name}```\n**Code:** ```{$this->code}```\n**Message:** ```{$this->error}```\n**Trace:** ```{$this->trace}```\n**Request:** `{$this->requestMethod} {$this->requestUri}`\n"
         );
 
         return "https://github.com/{$this->repo}/issues/new?title={$title}&body={$body}";
@@ -80,33 +111,82 @@ class GithubReport
 
     /**
      * @param string $name
+     * @return GithubReport
      */
-    public function setName(string $name) : void
+    public function setName(string $name): GithubReport
     {
         $this->name = $name;
+        return $this;
     }
 
     /**
      * @param string $code
+     * @return GithubReport
      */
-    public function setCode(string $code) : void
+    public function setCode(string $code): GithubReport
     {
         $this->code = $code;
+        return $this;
     }
 
     /**
      * @param string $error
+     * @return GithubReport
      */
-    public function setError(string $error) : void
+    public function setError(string $error): GithubReport
     {
         $this->error = $error;
+        return $this;
     }
 
     /**
      * @param string $repo
+     * @return GithubReport
      */
-    public function setRepo(string $repo) : void
+    public function setRepo(string $repo): GithubReport
     {
         $this->repo = $repo;
+        return $this;
     }
+
+    /**
+     * @param string $trace
+     * @return GithubReport
+     */
+    public function setTrace(string $trace): GithubReport
+    {
+        $this->trace = $trace;
+        return $this;
+    }
+
+    /**
+     * @param string $jikanVersion
+     * @return GithubReport
+     */
+    public function setJikanVersion(string $jikanVersion): GithubReport
+    {
+        $this->jikanVersion = $jikanVersion;
+        return $this;
+    }
+
+    /**
+     * @param string $redisRunning
+     * @return GithubReport
+     */
+    public function setRedisRunning(string $redisRunning): GithubReport
+    {
+        $this->redisRunning = $redisRunning;
+        return $this;
+    }
+
+    /**
+     * @param string $instanceType
+     * @return GithubReport
+     */
+    public function setInstanceType(string $instanceType): GithubReport
+    {
+        $this->instanceType = $instanceType;
+        return $this;
+    }
+
 }
