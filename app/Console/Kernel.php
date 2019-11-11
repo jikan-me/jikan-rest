1c17fa942f87a8db91210b7dc3cac7953a044613
Irfan <irfan.dahir@gmaill.com>
2019-11-11 05:16:35 -0800

    add command: change cache driver #63

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ModifyCacheDriver;
 use App\Console\Commands\ModifyCacheMethod;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        ModifyCacheMethod::class
+        ModifyCacheMethod::class,
+        ModifyCacheDriver::class
     ];
 
     /**
