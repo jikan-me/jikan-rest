473ba724de699dfd1a79a7cc6d20b18b51696749
Irfan <irfan.dahir@gmaill.com>
2019-12-25 20:18:58 +0500

    🤔

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 */
 defined('BLACKLIST_PATH') or define('BLACKLIST_PATH', __DIR__.'/../storage/app/blacklist.json');
 defined('JIKAN_PARSER_VERSION') or define('JIKAN_PARSER_VERSION', Versions::getVersion('jikan-me/jikan'));
-defined('JIKAN_REST_API_VERSION') or define('JIKAN_REST_API_VERSION', '3.4');
+defined('JIKAN_REST_API_VERSION') or define('JIKAN_REST_API_VERSION', '4');
 
 
 /*
@@ -32,6 +32,7 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
+$app->register(Jenssegers\Mongodb\MongodbServiceProvider::class);
 $app->withFacades();
 $app->withEloquent();
 
--- config/database.php
+++ config/database.php
@@ -1,6 +1,25 @@
 <?php
 
+/**
+ * Redis Caching
+ */
 return [
+    'default' => 'mongodb',
+    'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options' => [
+                'database' => 'admin' // sets the authentication database required by mongo 3
+            ]
+        ],
+    ],
+    'migrations' => 'migrations',
+
     'redis' => [
         'client' => 'predis',
         'default' => [
