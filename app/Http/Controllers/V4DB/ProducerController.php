318dfb422e06924eb0137848fb46ad13ad629071
Irfan <irfan.dahir@gmaill.com>
2020-06-19 11:59:10 +0500

    v4 DB add Producers

--- app/Http/Controllers/V4DB/ProducerController.php
+++ app/Http/Controllers/V4DB/ProducerController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers\V4DB;
 
+use App\Anime;
+use App\Http\Resources\V4\AnimeCollection;
+use Illuminate\Http\Request;
 use Jikan\Request\Producer\ProducerRequest;
 use Jikan\Request\Producer\ProducersRequest;
 
 class ProducerController extends Controller
 {
+
+    private $request;
+    const MAX_RESULTS_PER_PAGE = 50;
 
     public function main()
     {
@@ -14,9 +20,25 @@ class ProducerController extends Controller
         return response($this->serializer->serialize($results, 'json'));
     }
 
-    public function resource(int $id, int $page = 1)
+    public function resource(Request $request, int $id)
     {
-        $producer = $this->jikan->getProducer(new ProducerRequest($id, $page));
-        return response($this->serializer->serialize($producer, 'json'));
+        $this->request = $request;
+        $page = $this->request->get('page') ?? 1;
+
+        $results = Anime::query()
+            ->where('producers.mal_id', $id)
+            ->orderBy('title');
+
+        $results = $results
+            ->paginate(
+                self::MAX_RESULTS_PER_PAGE,
+                ['*'],
+                null,
+                $page
+            );
+
+        return new AnimeCollection(
+            $results
+        );
     }
 }
