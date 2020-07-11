3a2a9b06ce09151db38a84b4ca0aeed23341c80b
Irfan <irfan.dahir@gmaill.com>
2020-07-11 12:15:06 +0500

    Create 2020_07_11_121125_create_producers_table.php

---
+++ database/migrations/2020_07_11_121125_create_producers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producers', function (Blueprint $table) {
+            $table->unique(['request_hash' => 1], 'request_hash');
+            $table->unique(['mal_id' => 1], 'mal_id');
+            $table->index('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producers');
+    }
+}
