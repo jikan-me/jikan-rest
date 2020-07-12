c28818c4ec1c4548dd58ade1d9d07dca1a3e3069
Irfan <irfan.dahir@gmaill.com>
2020-07-12 14:08:48 +0500

    add v4 user clubs

--- app/Http/Controllers/V4DB/UserController.php
+++ app/Http/Controllers/V4DB/UserController.php
@@ -365,15 +365,60 @@ class UserController extends Controller
         );
     }
 
-    public function clubs(string $username)
+    public function clubs(Request $request, string $username)
     {
-        $results = [
-            'clubs' => $this->jikan->getUserClubs(
-                new UserClubsRequest($username)
-            )
-        ];
 
-        return response($this->serializer->serialize($results, 'json'));
+        $results = DB::table($this->getRouteTable($request))
+            ->where('request_hash', $this->fingerprint)
+            ->get();
+
+        if (
+            $results->isEmpty()
+            || $this->isExpired($request, $results)
+        ) {
+            $data = ['results' => $this->jikan->getUserClubs(new UserClubsRequest($username))];
+            $response = \json_decode($this->serializer->serialize($data, 'json'), true);
+
+            if (HttpHelper::hasError($response)) {
+                return HttpResponse::notFound($request);
+            }
+
+            if ($results->isEmpty()) {
+                $meta = [
+                    'createdAt' => new UTCDateTime(),
+                    'modifiedAt' => new UTCDateTime(),
+                    'request_hash' => $this->fingerprint
+                ];
+            }
+            $meta['modifiedAt'] = new UTCDateTime();
+
+            $response = $meta + $response;
+
+            if ($results->isEmpty()) {
+                DB::table($this->getRouteTable($request))
+                    ->insert($response);
+            }
+
+            if ($this->isExpired($request, $results)) {
+                DB::table($this->getRouteTable($request))
+                    ->where('request_hash', $this->fingerprint)
+                    ->update($response);
+            }
+
+            $results = DB::table($this->getRouteTable($request))
+                ->where('request_hash', $this->fingerprint)
+                ->get();
+        }
+
+        $response = (new ResultsResource(
+            $results->first()
+        ))->response();
+
+        return $this->prepareResponse(
+            $response,
+            $results,
+            $request
+        );
     }
 
     public function recentlyOnline()
