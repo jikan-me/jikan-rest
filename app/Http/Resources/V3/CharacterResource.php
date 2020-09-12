6bd07cb2653ce316c009844f6b0b0202c14075ea
Irfan <irfan.dahir@gmaill.com>
2020-09-13 01:59:14 +0500

    bc misc. fixes

--- app/Http/Resources/V3/CharacterResource.php
+++ app/Http/Resources/V3/CharacterResource.php
@@ -21,11 +21,57 @@ class CharacterResource extends JsonResource
             'image_url' => $this->images['jpg']['image_url'],
             'name' => $this->name,
             'nicknames' => $this->nicknames,
-            'favorites' => $this->favorites,
+            'member_favorites' => $this->favorites,
             'about' => $this->about,
-            'animeography' => $this->animeography,
-            'mangaography' => $this->mangaography,
-            'voice_actors' => $this->voice_actors
+            'animeography' => $this->bcAnimeography($this->animeography),
+            'mangaography' => $this->bcMangaography($this->mangaography),
+            'voice_actors' => $this->bcVoiceActors($this->voice_actors)
         ];
     }
+
+    private function bcAnimeography($ography) : array
+    {
+        foreach ($ography as &$entry) {
+            $entry = [
+                'mal_id' => $entry['anime']['mal_id'],
+                'name' => $entry['anime']['title'],
+                'url' => $entry['anime']['url'],
+                'image_url' => $entry['anime']['images']['jpg']['image_url'],
+                'role' => $entry['role'],
+            ];
+        }
+
+        return $ography;
+    }
+
+    private function bcMangaography($ography) : array
+    {
+        foreach ($ography as &$entry) {
+            $entry = [
+                'mal_id' => $entry['manga']['mal_id'],
+                'name' => $entry['manga']['title'],
+                'url' => $entry['manga']['url'],
+                'image_url' => $entry['manga']['images']['jpg']['image_url'],
+                'role' => $entry['role'],
+            ];
+        }
+
+        return $ography;
+    }
+
+    private function bcVoiceActors($voiceActors) : array
+    {
+        foreach ($voiceActors as &$person) {
+            $person = [
+                'mal_id' => $person['person']['mal_id'],
+                'name' => $person['person']['name'],
+                'url' => $person['person']['url'],
+                'image_url' => $person['person']['images']['jpg']['image_url'],
+                'language' => $person['language'],
+            ];
+        }
+
+        return $voiceActors;
+    }
+
 }
--- app/Http/Resources/V3/PersonResource.php
+++ app/Http/Resources/V3/PersonResource.php
@@ -26,9 +26,66 @@ class PersonResource extends JsonResource
             'birthday' => $this->birthday,
             'member_favorites' => $this->favorites,
             'about' => $this->about,
-            'voice_acting_roles' => $this->voice_acting_roles,
-            'anime_staff_positions' => $this->anime_staff_positions,
-            'published_manga' => $this->published_manga,
+            'voice_acting_roles' => $this->bcVoiceActingRoles($this->voice_acting_roles),
+            'anime_staff_positions' => $this->bcAnimeStaffPositions($this->anime_staff_positions),
+            'published_manga' => $this->bcPublishedManga($this->published_manga),
         ];
+    }
+
+    private function bcVoiceActingRoles($voiceActingRoles) : array
+    {
+        foreach ($voiceActingRoles as &$voiceActor) {
+            $voiceActor = [
+                'role' => $voiceActor['role'],
+                'anime' => [
+                    'mal_id' => $voiceActor['anime']['mal_id'],
+                    'url' => $voiceActor['anime']['url'],
+                    'image_url' => $voiceActor['anime']['images']['jpg']['image_url'],
+                    'name' => $voiceActor['anime']['title'],
+                ],
+                'character' => [
+                    'mal_id' => $voiceActor['character']['mal_id'],
+                    'url' => $voiceActor['character']['url'],
+                    'image_url' => $voiceActor['character']['images']['jpg']['image_url'],
+                    'name' => $voiceActor['character']['name'],
+                ],
+            ];
+        }
+
+        return $voiceActingRoles;
+    }
+
+    private function bcAnimeStaffPositions($animeStaffPositions) : array
+    {
+        foreach ($animeStaffPositions as &$anime) {
+            $anime = [
+                'position' => $anime['position'],
+                'anime' => [
+                    'mal_id' => $anime['anime']['mal_id'],
+                    'url' => $anime['anime']['url'],
+                    'image_url' => $anime['anime']['images']['jpg']['image_url'],
+                    'name' => $anime['anime']['title'],
+                ],
+            ];
+        }
+
+        return $animeStaffPositions;
+    }
+
+    private function bcPublishedManga($publishedManga) : array
+    {
+        foreach ($publishedManga as &$manga) {
+            $manga = [
+                'position' => $manga['position'],
+                'manga' => [
+                    'mal_id' => $manga['manga']['mal_id'],
+                    'url' => $manga['manga']['url'],
+                    'image_url' => $manga['manga']['images']['jpg']['image_url'],
+                    'name' => $manga['manga']['title'],
+                ],
+            ];
+        }
+
+        return $publishedManga;
     }
 }
