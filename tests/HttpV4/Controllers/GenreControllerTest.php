37499f2efa1140a9b6d0b953b1a1ea0c762ff2a5
Irfan <irfan.dahir@gmaill.com>
2021-01-14 06:35:56 +0500

    Update Genres Test

--- tests/HttpV4/Controllers/GenreControllerTest.php
+++ tests/HttpV4/Controllers/GenreControllerTest.php
@@ -4,109 +4,35 @@ class GenreControllerTest extends TestCase
 {
     public function testAnimeGenre()
     {
-        $this->get('/v3/genre/anime/1')
+        $this->get('/v4/genres/anime')
             ->seeStatusCode(200)
-            ->seeJsonStructure([
-                'mal_url' => [
+            ->seeJsonStructure(['data'=>[
+                [
                     'mal_id',
-                    'type',
                     'name',
-                    'url'
-                ],
-                'item_count',
-                'anime' => [
-                    [
-                        'mal_id',
-                        'url',
-                        'title',
-                        'image_url',
-                        'synopsis',
-                        'type',
-                        'airing_start',
-                        'episodes',
-                        'members',
-                        'genres' => [
-                            [
-                                'mal_id',
-                                'type',
-                                'name',
-                                'url'
-                            ]
-                        ],
-                        'source',
-                        'producers' => [
-                            [
-                                'mal_id',
-                                'type',
-                                'name',
-                                'url'
-                            ]
-                        ],
-                        'score',
-                        'licensors',
-                        'r18',
-                        'kids'
-                    ]
+                    'url',
+                    'count'
                 ]
-            ]);
+            ]]);
     }
 
     public function testMangaGenre()
     {
-        $this->get('/v3/genre/manga/1')
+        $this->get('/v4/genres/manga')
             ->seeStatusCode(200)
-            ->seeJsonStructure([
-                'mal_url' => [
+            ->seeJsonStructure(['data'=>[
+                [
                     'mal_id',
-                    'type',
                     'name',
-                    'url'
-                ],
-                'item_count',
-                'manga' => [
-                    [
-                        'mal_id',
-                        'url',
-                        'title',
-                        'image_url',
-                        'synopsis',
-                        'type',
-                        'publishing_start',
-                        'volumes',
-                        'members',
-                        'genres' => [
-                            [
-                                'mal_id',
-                                'type',
-                                'name',
-                                'url'
-                            ]
-                        ],
-                        'authors' => [
-                            [
-                                'mal_id',
-                                'type',
-                                'name',
-                                'url'
-                            ]
-                        ],
-                        'score',
-                        'serialization',
-//                        'r18', todo ?
-                    ]
+                    'url',
+                    'count'
                 ]
-            ]);
+            ]]);
     }
 
     public function test404()
     {
-        $this->get('/v3/genre/anime/1/1000')
-            ->seeStatusCode(404);
-        $this->get('/v3/genre/manga/1/1000')
-            ->seeStatusCode(404);
-        $this->get('/v3/genre/anime/100')
-            ->seeStatusCode(404);
-        $this->get('/v3/genre/manga/100')
+        $this->get('/v4/genres')
             ->seeStatusCode(404);
     }
 }
