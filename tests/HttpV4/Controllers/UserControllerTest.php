779bab7075522069afd2addc41e3b2d2fe9d9f71
Irfan <irfan.dahir@gmaill.com>
2021-01-14 07:06:42 +0500

    Update User Tests

--- tests/HttpV4/Controllers/UserControllerTest.php
+++ tests/HttpV4/Controllers/UserControllerTest.php
@@ -4,81 +4,218 @@ class UserControllerTest extends TestCase
 {
     public function testUserProfile()
     {
-        $this->get('/v3/user/nekomata1037')
+        $this->get('/v4/users/nekomata1037')
             ->seeStatusCode(200)
-            ->seeJsonStructure([
+            ->seeJsonStructure(['data'=>[
+                'mal_id',
                 'username',
                 'url',
-                'image_url',
+                'images' => [
+                    'jpg' => [
+                        'image_url'
+                    ],
+                    'webp' => [
+                        'image_url'
+                    ]
+                ],
                 'last_online',
                 'gender',
                 'birthday',
                 'location',
                 'joined',
-                'anime_stats' => [
-                    'days_watched',
-                    'mean_score',
-                    'watching',
-                    'completed',
-                    'on_hold',
-                    'dropped',
-                    'plan_to_watch',
-                    'total_entries',
-                    'rewatched',
-                    'episodes_watched'
-                ],
-                'manga_stats' => [
-                    'days_read',
-                    'mean_score',
-                    'reading',
-                    'completed',
-                    'on_hold',
-                    'dropped',
-                    'plan_to_read',
-                    'total_entries',
-                    'reread',
-                    'chapters_read',
-                    'volumes_read'
-                ],
-                'favorites' => [
-                    'anime' => [
-                        [
-                            'mal_id',
-                            'url',
-                            'image_url',
-                            'name' // todo should be `title`
-                        ]
-                    ],
-                    'manga' => [],
-                    'characters' => [
-                        [
-                            'mal_id',
-                            'url',
-                            'image_url',
-                            'name'
-                        ]
-                    ],
-                    'people' => [
-                        [
-                            'mal_id',
-                            'url',
-                            'image_url',
-                            'name'
-                        ]
-                    ],
-                ],
-                'about'
-            ]);
+                /* 'anime_stats' => [
+                     'days_watched',
+                     'mean_score',
+                     'watching',
+                     'completed',
+                     'on_hold',
+                     'dropped',
+                     'plan_to_watch',
+                     'total_entries',
+                     'rewatched',
+                     'episodes_watched'
+                 ],
+                 'manga_stats' => [
+                     'days_read',
+                     'mean_score',
+                     'reading',
+                     'completed',
+                     'on_hold',
+                     'dropped',
+                     'plan_to_read',
+                     'total_entries',
+                     'reread',
+                     'chapters_read',
+                     'volumes_read'
+                 ],
+                 'favorites' => [
+                     'anime' => [
+                         [
+                             'mal_id',
+                             'url',
+                             'image_url',
+                             'name' // todo should be `title`
+                         ]
+                     ],
+                     'manga' => [],
+                     'characters' => [
+                         [
+                             'mal_id',
+                             'url',
+                             'image_url',
+                             'name'
+                         ]
+                     ],
+                     'people' => [
+                         [
+                             'mal_id',
+                             'url',
+                             'image_url',
+                             'name'
+                         ]
+                     ],
+                 ],
+                 'about'*/
+            ]]);
     }
+
+    public function testUserStatistics()
+    {
+        $this->get('/v4/users/nekomata1037/statistics')
+            ->seeStatusCode(200)
+            ->seeJsonStructure(['data'=>[
+
+                'anime' => [
+                     'days_watched',
+                     'mean_score',
+                     'watching',
+                     'completed',
+                     'on_hold',
+                     'dropped',
+                     'plan_to_watch',
+                     'total_entries',
+                     'rewatched',
+                     'episodes_watched'
+                 ],
+                 'manga' => [
+                     'days_read',
+                     'mean_score',
+                     'reading',
+                     'completed',
+                     'on_hold',
+                     'dropped',
+                     'plan_to_read',
+                     'total_entries',
+                     'reread',
+                     'chapters_read',
+                     'volumes_read'
+                 ],
+            ]]);
+    }
+
+    public function testUserAbout()
+    {
+        $this->get('/v4/users/nekomata1037/about')
+            ->seeStatusCode(200)
+            ->seeJsonStructure(['data'=>[
+                'about',
+            ]]);
+    }
+
+    public function testUserFavorites()
+    {
+        $this->get('/v4/users/nekomata1037/favorites')
+            ->seeStatusCode(200)
+            ->seeJsonStructure(['data'=>[
+                'anime' => [
+                    [
+                        'mal_id',
+                        'url',
+                        'images' => [
+                            'jpg' => [
+                                'image_url',
+                                'small_image_url',
+                                'large_image_url'
+                            ],
+                            'webp' => [
+                                'image_url',
+                                'small_image_url',
+                                'large_image_url'
+                            ],
+                        ],
+                        'title',
+                        'type',
+                        'start_year'
+                    ]
+                ],
+                'manga' => [
+                    [
+                        'mal_id',
+                        'url',
+                        'images' => [
+                            'jpg' => [
+                                'image_url',
+                                'small_image_url',
+                                'large_image_url'
+                            ],
+                            'webp' => [
+                                'image_url',
+                                'small_image_url',
+                                'large_image_url'
+                            ],
+                        ],
+                        'title',
+                        'type',
+                        'start_year'
+                    ]
+                ],
+                'characters' => [
+                    [
+                        'mal_id',
+                        'url',
+                        'images' => [
+                            'jpg' => [
+                                'image_url',
+                            ],
+                            'webp' => [
+                                'image_url',
+                            ],
+                        ],
+                        'name',
+                        'entry' => [
+                            'mal_id',
+                            'title',
+                            'type',
+                            'url'
+                        ]
+                    ]
+                ],
+                'people' => [
+                    [
+                        'mal_id',
+                        'url',
+                        'images' => [
+                            'jpg' => [
+                                'image_url',
+                            ],
+                        ],
+                        'name',
+                    ]
+                ]
+
+            ]]);
+    }
+
+
 
     public function testUserHistory()
     {
-        $this->get('/v3/user/nekomata1037/history')
+        $this->get('/v4/users/purplepinapples/history/anime')
             ->seeStatusCode(200)
             ->seeJsonStructure([
-                'history' => [
+                'data' => [
                     [
-                        'meta' => [
+                        'entry' => [
                             'mal_id',
                             'type',
                             'name',
@@ -89,75 +226,101 @@ class UserControllerTest extends TestCase
                     ]
                 ]
             ]);
+
+        $this->get('/v4/users/nekomata1037/history/manga')
+            ->seeStatusCode(200)
+            ->seeJsonStructure([
+                'data' => [
+
+                ]
+            ]);
     }
 
     public function testUserFriends()
     {
-        $this->get('/v3/user/nekomata1037/friends')
+        $this->get('/v4/users/nekomata1037/friends')
             ->seeStatusCode(200)
             ->seeJsonStructure([
-                'friends' => [
+                'meta' => [
+                    'last_visible_page',
+                    'hast_next_page',
+                ],
+                'data' => [
                     [
-                        'url',
-                        'username',
-                        'image_url',
+                        'user' => [
+                            'username',
+                            'url',
+                            'images'
+                        ],
                         'last_online',
                         'friends_since'
                     ]
                 ]
             ]);
+
+        $this->get('/v4/users/nekomata1037/friends?page=200')
+            ->seeStatusCode(404);
     }
 
     public function testUserAnimeList()
     {
-        $this->get('/v3/user/nekomata1037/animelist?order_by=last_updated&sort=descending')
+        $this->get('/v4/users/nekomata1037/animelist?order_by=last_updated&sort=descending')
             ->seeStatusCode(200)
             ->seeJsonStructure([
-                'anime' => [
+                'data' => [
                     [
-                        'mal_id',
-                        'title',
-                        'video_url',
-                        'url',
-                        'image_url',
-                        'type',
                         'watching_status',
                         'score',
-                        'watched_episodes',
-                        'total_episodes',
-                        'airing_status',
-                        'season_name',
-                        'season_year',
-                        'has_episode_video',
-                        'has_promo_video',
-                        'has_video',
-                        'is_rewatching',
+                        'episodes_watched',
                         'tags',
-                        'rating',
-                        'start_date',
-                        'end_date',
+                        'is_rewatching',
                         'watch_start_date',
                         'watch_end_date',
                         'days',
                         'storage',
                         'priority',
-                        'added_to_list',
-                        'studios' => [
-                            [
-                                'mal_id',
-                                'type',
-                                'name',
-                                'url'
-                            ]
+                        'anime' => [
+                            'mal_id',
+                            'title',
+                            'url',
+                            'images' => [
+                                'jpg' => [
+                                    'image_url',
+                                    'small_image_url',
+                                    'large_image_url'
+                                ],
+                                'webp' => [
+                                    'image_url',
+                                    'small_image_url',
+                                    'large_image_url'
+                                ],
+                            ],
                         ],
-                        'licensors' => [
-                            [
-                                'mal_id',
-                                'type',
-                                'name',
-                                'url'
-                            ]
+                        'type',
+                        'season',
+                        'year',
+                        'episodes',
+                        'rating',
+                        'airing',
+                        'aired' => [
+                            'from',
+                            'to',
+                            'prop' => [
+                                'from' => [
+                                    'day',
+                                    'month',
+                                    'year'
+                                ],
+                                'to' => [
+                                    'day',
+                                    'month',
+                                    'year'
+                                ]
+                            ],
+                            'string'
                         ],
+                        'studios',
+                        'licensors'
                     ]
                 ]
             ]);
@@ -165,7 +328,68 @@ class UserControllerTest extends TestCase
 
     public function testUserMangaList()
     {
-        $this->get('/v3/user/nekomata1037/mangalist?order_by=last_updated&sort=descending')
+        $this->get('/v4/users/purplepinapples/mangalist?order_by=last_updated&sort=descending')
+            ->seeStatusCode(200)
+            ->seeJsonStructure([
+                'data' => [
+                    [
+                        'reading_status',
+                        'score',
+                        'chapters_read',
+                        'tags',
+                        'is_rereading',
+                        'read_start_date',
+                        'read_end_date',
+                        'days',
+                        'retail',
+                        'priority',
+                        'manga' => [
+                            'mal_id',
+                            'title',
+                            'url',
+                            'images' => [
+                                'jpg' => [
+                                    'image_url',
+                                    'small_image_url',
+                                    'large_image_url'
+                                ],
+                                'webp' => [
+                                    'image_url',
+                                    'small_image_url',
+                                    'large_image_url'
+                                ],
+                            ],
+                        ],
+                        'type',
+                        'chapters',
+                        'volumes',
+                        'publishing',
+                        'published' => [
+                            'from',
+                            'to',
+                            'prop' => [
+                                'from' => [
+                                    'day',
+                                    'month',
+                                    'year'
+                                ],
+                                'to' => [
+                                    'day',
+                                    'month',
+                                    'year'
+                                ]
+                            ],
+                            'string'
+                        ],
+                        'magazines'
+                    ]
+                ]
+            ]);
+    }
+
+    public function testUserPrivateList()
+    {
+        $this->get('/v4/user/nekomata1037/mangalist?order_by=last_updated&sort=descending')
             ->seeStatusCode(400);
     }
 }
