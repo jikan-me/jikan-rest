938312c3322eb5f99ce1e923916d0258630b2829
Irfan <irfan-dahir@users.noreply.github.com>
2021-11-03 17:29:48 +0500

    remove: patreon api not reliable

--- routes/web.v4.php
+++ routes/web.v4.php
@@ -22,29 +22,6 @@ $router->get('/', function () use ($router) {
     ]);
 });
 
-/*$router->group( @todo secure sensitive endpoints
-    [
-        'prefix' => 'patreon'
-    ],
-    function () use ($router) {
-        $router->get('/oauth', [
-            'uses' => 'PatreonController@login'
-        ]);
-
-        $router->get('/url', [
-            'uses' => 'PatreonController@url'
-        ]);
-
-        $router->get('/campaigns', [
-            'uses' => 'PatreonController@campaigns'
-        ]);
-
-        $router->get('/pledges', [
-            'uses' => 'PatreonController@pledges'
-        ]);
-    }
-);*/
-
 $router->get('/anime', [
     'uses' => 'SearchController@anime'
 ]);
