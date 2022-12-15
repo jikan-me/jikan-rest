557016f09078046702e43ec607005870a4d3643a
pushrbx <contact@pushrbx.net>
2022-12-15 15:57:12 +0000

    refactored test arrange code

--- app/Testing/Concerns/MakesHttpRequestsEx.php
+++ app/Testing/Concerns/MakesHttpRequestsEx.php
@@ -3,6 +3,11 @@ namespace App\Testing\Concerns;
 
 trait MakesHttpRequestsEx
 {
+    protected function getBaseUri(): string
+    {
+        return "/";
+    }
+
     /**
      * Visit the given URI with a JSON GET request.
      * @param string $uri
@@ -12,5 +17,17 @@ trait MakesHttpRequestsEx
     public function getJson(string $uri, array $headers = []): self
     {
         return $this->json('GET', $uri, [], $headers);
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     */
+    public function getJsonResponse(array $params): array
+    {
+        $parameters = http_build_query($params);
+        $uri = $this->getBaseUri() . "?" . $parameters;
+        $this->getJson($uri);
+        return $this->response->json();
     }
 }
