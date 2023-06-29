4cf1fb2926f4fdeaf160140a22015a9551aa530a
pushrbx <contact@pushrbx.net>
2023-06-29 21:02:28 +0100

    max results per page improvement

--- app/Rules/Attributes/MaxLimitWithFallback.php
+++ app/Rules/Attributes/MaxLimitWithFallback.php
@@ -5,13 +5,12 @@ namespace App\Rules\Attributes;
 use App\Rules\MaxResultsPerPageRule;
 use Attribute;
 use Spatie\LaravelData\Attributes\Validation\Rule;
-use Illuminate\Support\Facades\App;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 final class MaxLimitWithFallback extends Rule
 {
     public function __construct()
     {
-        parent::__construct(new MaxResultsPerPageRule(App::make("jikan-config")->maxResultsPerPage()));
+        parent::__construct(new MaxResultsPerPageRule(max_results_per_page()));
     }
 }
